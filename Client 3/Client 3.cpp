c999dba663d8f61bc7df71a1affab34efa9f4cde
memak <memaksim5@gmail.com>
2024-11-05 11:30:49 +0200

    Client 3

--- Client 3/Client 3.cpp
+++ Client 3/Client 3.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿#include "GraphicsLib.h"
+#include <iostream>
 #include <vector>
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
@@ -14,50 +15,127 @@ enum CommandOpcode {
     FILL_ELLIPSE_OPCODE
 };
 
-// Функція для відправки байтового масиву на сервер
-void sendCommand(SOCKET& clientSocket, sockaddr_in& serverAddr, const std::vector<uint8_t>& command) {
-    sendto(clientSocket, (char*)command.data(), command.size(), 0,
-        (sockaddr*)&serverAddr, sizeof(serverAddr));
-}
+class DisplayClient : public GraphicsLib {
+public:
+    DisplayClient(uint_least16_t w, uint_least16_t h) : GraphicsLib(w, h) {
+        initSocket();
+    }
+
+    ~DisplayClient() {
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+    void fillScreen(uint_least16_t color) override {
+       
+        std::vector<uint8_t> command = { CLEAR_DISPLAY_OPCODE };
+        addColorToCommand(command, color);
+        sendCommand(command);
+    }
+
+    void drawPixel(int_least16_t x0, int_least16_t y0, uint_least16_t color) override {
+        std::vector<uint8_t> command = { DRAW_PIXEL_OPCODE };
+        addCoordinatesToCommand(command, x0, y0);
+        addColorToCommand(command, color);
+        sendCommand(command);
+       
+    }
+
+    void drawLine(int_least16_t x0, int_least16_t y0, int_least16_t x1, int_least16_t y1, uint_least16_t color) override {
+        std::vector<uint8_t> command = { DRAW_LINE_OPCODE };
+        addCoordinatesToCommand(command, x0, y0);
+        addCoordinatesToCommand(command, x1, y1);
+        addColorToCommand(command, color);
+        sendCommand(command);
+       
+    }
+
+    void drawRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) override {
+        std::vector<uint8_t> command = { DRAW_RECTANGLE_OPCODE };
+        addCoordinatesToCommand(command, x0, y0);
+        addCoordinatesToCommand(command, x0 + w, y0 + h);
+        addColorToCommand(command, color);
+        sendCommand(command);
+    }
+
+    void fillRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) override {
+        std::vector<uint8_t> command = { FILL_RECTANGLE_OPCODE };
+        addCoordinatesToCommand(command, x0, y0);
+        addCoordinatesToCommand(command, x0 + w, y0 + h);
+        addColorToCommand(command, color);
+        sendCommand(command);
+    }
+
+    void drawEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) override {
+        std::vector<uint8_t> command = { DRAW_ELLIPSE_OPCODE };
+        addCoordinatesToCommand(command, x0, y0);
+        addCoordinatesToCommand(command, r_x, r_y);
+        addColorToCommand(command, color);
+        sendCommand(command);
+    }
+
+    void fillEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) override {
+        std::vector<uint8_t> command = { FILL_ELLIPSE_OPCODE };
+        addCoordinatesToCommand(command, x0, y0);
+        addCoordinatesToCommand(command, r_x, r_y);
+        addColorToCommand(command, color);
+        sendCommand(command);
+    }
+
+private:
+    SOCKET clientSocket;
+    sockaddr_in serverAddr;
+
+    void initSocket() {
+        WSAData wsaData;
+        WORD DLLVersion = MAKEWORD(2, 2);
+        if (WSAStartup(DLLVersion, &wsaData) != 0) {
+            throw std::runtime_error("Error initializing WinSock");
+        }
+        clientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        if (clientSocket == INVALID_SOCKET) {
+            WSACleanup();
+            throw std::runtime_error("Error creating socket");
+        }
+
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(1111); // Порт сервера
+        serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Локальний IP
+    }
+
+    void sendCommand(const std::vector<uint8_t>& command) {
+        sendto(clientSocket, (char*)command.data(), command.size(), 0,
+            (sockaddr*)&serverAddr, sizeof(serverAddr));
+    }
+
+    void addColorToCommand(std::vector<uint8_t>& command, uint16_t color) {
+        command.push_back((color >> 8) & 0xFF);
+        command.push_back(color & 0xFF);
+    }
+
+    void addCoordinatesToCommand(std::vector<uint8_t>& command, int16_t x, int16_t y) {
+        command.push_back((x >> 8) & 0xFF);
+        command.push_back(x & 0xFF);
+        command.push_back((y >> 8) & 0xFF);
+        command.push_back(y & 0xFF);
+    }
+};
+
 
 int main() {
-    // Ініціалізація WinSock
-    WSAData wsaData;
-    WORD DLLVersion = MAKEWORD(2, 2);
-    if (WSAStartup(DLLVersion, &wsaData) != 0) {
-        std::cerr << "Error initializing WinSock" << std::endl;
-        return -1;
+    try {
+        DisplayClient display(800, 600);
+        display.fillScreen(RGB(0, 0,0)); 
+        display.drawPixel(100, 100, RGB(255, 0, 0)); 
+        display.drawLine(100, 50, 150, 150, RGB(0, 255, 0)); 
+        display.drawRect(200, 200, 200, 200, RGB(0, 0, 255)); 
+        display.fillRect(200, 200, 200, 200, RGB(255, 255, 0)); 
+        display.drawEllipse(400, 400, 30, 15, RGB(255, 0, 255)); 
+        display.fillEllipse(400, 400, 30, 15, RGB(0, 255, 255)); 
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
     }
 
-    // Налаштування сокета клієнта
-    SOCKET clientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (clientSocket == INVALID_SOCKET) {
-        std::cerr << "Error creating socket" << std::endl;
-        WSACleanup();
-        return -1;
-    }
-
-    sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(1111); // Порт сервера
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Локальний IP
-
-    std::vector<std::vector<uint8_t>> commandList = {
-        { CLEAR_DISPLAY_OPCODE, 0xFF, 0xFF },
-        { DRAW_PIXEL_OPCODE, 0x00, 0x10, 0x00, 0x20, 0xAA, 0xBB },
-        { DRAW_LINE_OPCODE, 0x00, 0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x40, 0xCC, 0xDD },
-        { DRAW_RECTANGLE_OPCODE, 0x00, 0x05, 0x00, 0x10, 0x00, 0x15, 0x00, 0x20, 0xEE, 0xFF },
-        { FILL_RECTANGLE_OPCODE, 0x00, 0x05, 0x00, 0x10, 0x00, 0x15, 0x00, 0x20, 0x11, 0x22 },
-        { DRAW_ELLIPSE_OPCODE, 0x00, 0x08, 0x00, 0x12, 0x00, 0x09, 0x00, 0x07, 0x33, 0x44 },
-        { FILL_ELLIPSE_OPCODE, 0x00, 0x06, 0x00, 0x11, 0x00, 0x05, 0x00, 0x04, 0x55, 0x66 },
-    };
-
-    for (const auto& command : commandList) {
-        sendCommand(clientSocket, serverAddr, command);
-        std::cout << "Command sent to server." << std::endl;
-    }
-
-    closesocket(clientSocket);
-    WSACleanup();
     return 0;
 }
---
+++ Client 3/GraphicsLib.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef GraphicsLib_h
+#define GraphicsLib_h
+
+#include <stdint.h>
+
+#ifndef RGB
+#define RGB(r,g,b) (((r&0xF8)<<8)|((g&0xFC)<<3)|((b&0xF8)>>3)) // RGB565: 5 red | 6 green | 5 blue
+#endif
+
+class GraphicsLib
+{
+public:
+    GraphicsLib(uint_least16_t w, uint_least16_t h) : width(w), height(h) {}
+    GraphicsLib() : width(0), height(0) {}
+
+    int_least16_t getWidth(void) { return this->width; }
+    int_least16_t getHeight(void) { return this->height; }
+
+    virtual void fillScreen(uint_least16_t color) = 0;
+    virtual void drawPixel(int_least16_t x0, int_least16_t y0, uint_least16_t color) = 0;
+    virtual void drawLine(int_least16_t x0, int_least16_t y0, int_least16_t x1, int_least16_t y1, uint_least16_t color) = 0;
+    virtual void drawRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) = 0;
+    virtual void fillRect(int_least16_t x0, int_least16_t y0, int_least16_t w, int_least16_t h, uint_least16_t color) = 0;
+    virtual void drawEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) = 0;
+    virtual void fillEllipse(int_least16_t x0, int_least16_t y0, int_least16_t r_x, int_least16_t r_y, uint_least16_t color) = 0;
+
+private:
+    int_least16_t width, height; // screen size
+};
+
+#endif // GraphicsLib_h
